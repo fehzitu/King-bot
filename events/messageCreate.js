e876ebeae7c3bb17870e6e21f39552e405ce299b
fehzitu <fehzitujsdev@gmail.com>
2026-03-08 19:38:27 -0300

    add auto create user profile system

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,10 +1,45 @@
 const Discord = require('discord.js');
 
+// node file system
+const fs = require('fs');
+const path = require('path');
+
+// database json file
+const filePath = path.join(__dirname, '../database/users.json');
+
+// importint all custom functions
+const { loadJSON, saveJSON } = require(path.join(__dirname, '../functions/json.js'));
+
 module.exports = {
     name: Discord.Events.MessageCreate,
     async execute(message) {
         // check if an bot send the message
         if (message.author.bot) return;
+
+        // get user id
+        const userId = message.author.id;
+
+        // load the database file
+        const users = loadJSON(filePath);
+
+        // check if the users have a profile
+        if (!users[userId]) {
+            // create a new user data on the file
+            users[userId] = {
+                id: message.author.id,
+                name: message.author.username,
+                money: 100,
+                createdAt: new Date().toISOString(),
+                level: 0,
+                xp: 0
+            };
+
+            // save the data into a file
+            saveJSON(filePath, users);
+
+            // log
+            console.log(`🏆 Novo perfil criado para ${message.author.tag}`);
+        };
 
         // check the server and channel from the message
         const guildName = message.guild?.name ?? "DM";
--- functions/json.js
+++ functions/json.js
@@ -5,7 +5,7 @@ const writeQueue = new Map();
 
 // function to load and return all json file data
 function loadJSON(path, defaultData = {}) {
-	// try to create file if dont exist
+    // try to create file if dont exist
     try {
         if (!fs.existsSync(path)) {
             fs.writeFileSync(path, JSON.stringify(defaultData, null, 2));
@@ -13,10 +13,10 @@ function loadJSON(path, defaultData = {}) {
         };
 
         // get the file data and return
-        const data = fs.readFileSync(path, "utf8");
-        return JSON.parse(data);
+        const rawData = fs.readFileSync(path, "utf8");
+        return JSON.parse(rawData);
 
-	// catch error and return the original data
+        // catch error and return the original data
     } catch (err) {
         console.error("Erro ao carregar JSON:", err);
         return defaultData;
@@ -25,21 +25,21 @@ function loadJSON(path, defaultData = {}) {
 
 // function to create a json file
 function saveJSON(path, data) {
-	// format data to simple string
+    // format data to simple string
     const json = JSON.stringify(data, null, 2);
 
-	// resolve queue
+    // resolve queue
     const previous = writeQueue.get(path) || Promise.resolve();
 
-	// wait finish and perform next
+    // wait finish and perform next
     const next = previous.then(() => {
         return fs.promises.writeFile(path, json);
     });
 
-	// "continue" queue
+    // "continue" queue
     writeQueue.set(path, next);
 
-	// finish
+    // finish
     return next;
 };
 
