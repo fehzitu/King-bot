80bd77d30163708c6dfdfb59e2d315ef47264264
fehzitu <fehzitujsdev@gmail.com>
2026-04-01 18:23:23 -0300

    slap command fix

--- commands/prefixCommands/chat/slap.js
+++ commands/prefixCommands/chat/slap.js
@@ -2,99 +2,106 @@
 const Discord = require('discord.js');
 
 module.exports = {
+	// "name" will receive the value that will be the chat message that the bot captures as a command
 	name: 'tapa',
 	async execute(message) {
+		// define endpoint
+		let endpoint = 'slap';
+
+		// check if an bot has send the message
 		if (message.author.bot) return;
 
-		const content = message.content.toLowerCase().split(' ');
-		const firstMentionedUser = message.mentions.users.first();
+		// api anti-spam with 3 seconds
+		if (!message.client.cooldowns) {
+			message.client.cooldowns = new Set();
+		};
+
+		if (message.client.cooldowns.has(message.author.id)) return;
+
+		message.client.cooldowns.add(message.author.id);
+
+		setTimeout(() => {
+			message.client.cooldowns.delete(message.author.id);
+		}, 3000);
+
+		// set the user to a mentioned (if we have)
+		const user = message.mentions.users.first() || message.author;
+
+		// fallback image
+		const fallbackImage = 'https://cdn.discordapp.com/attachments/1477290272638632068/1488963005625663630/broken-image.png?ex=69ceb05c&is=69cd5edc&hm=42b8b9f7d66fc4746487d72ef0ea845bf5e9e2f662937d336752a7635855f09d';
 
 		// function to get the api img
 		const getImg = async (endpoint) => {
 			try {
 				// timer controller
 				const controller = new AbortController();
-				// 3 timeout
+				// 3 seconds timeout
 				const timeout = setTimeout(() => controller.abort(), 3000);
 
-				const res = await fetch(`https://api.waifu.pics/sfw/${endpoint}`, {
+				// get all the api data
+				const endpImg = await fetch(`https://api.waifu.pics/sfw/${endpoint}`, {
 					signal: controller.signal
 				});
+				const data = await endpImg.json();
 
+				// stop the timer
 				clearTimeout(timeout);
 
-				const data = await res.json();
-
+				// return the image link
 				return data.url;
-			} catch (err) {
-				console.error('Erro na API:', err);
-
-				// fallback image if api fails
-				return 'https://cdn.discordapp.com/attachments/1477290272638632068/1488963005625663630/broken-image.png?ex=69ceb05c&is=69cd5edc&hm=42b8b9f7d66fc4746487d72ef0ea845bf5e9e2f662937d336752a7635855f09d';
-			}
-		};
-
-		const errorEmbed = new Discord.MessageEmbed()
-			.setColor('RANDOM')
-			.setAuthor({
-				iconURL: message.author.displayAvatarURL(),
-				name: `@${message.author.username}`
-			})
-			.addFields([{
-				name: '🔴 **Uso incorreto do comando**!',
-				value: '(Faltou alguma marcação ou mais de 1 usuário foi marcado!)'
-			},
-			{
-				name: '🟢 **Uso correto**:',
-				value: 'k.tapa @[usuário]'
-			}])
-			.setTimestamp()
-			.setFooter({
-				text: 'Atualizado'
-			});
-
-		const successEmbed = new Discord.MessageEmbed()
-			.setColor('RANDOM')
-			.setAuthor({
-				iconURL: message.author.displayAvatarURL(),
-				name: `@${message.author.username}`
-			})
-			.setTimestamp()
-			.setFooter({
-				text: 'Atualizado'
-			});
-
-		let img;
-
-		// check correct usage
-		if (content.length === 2 && firstMentionedUser && message.mentions.users.size === 1) {
-
-			if (message.author.id === firstMentionedUser.id) {
-				// get the image
-				img = await getImg('cringe');
-
-				successEmbed.setDescription(
-					`🤔 **<@${message.author.id}> se odeia ao ponto de se bater**❓`
-				);
-			} else {
-				// get the image
-				img = await getImg('slap');
-
-				successEmbed.setDescription(
-					`😡 **<@${message.author.id}> deu um tapa em <@${firstMentionedUser.id}>**❗`
-				);
+			} catch (error) {
+				// fallback error img
+				return fallbackImage;
 			};
-
-			// set the image
-			successEmbed.setImage(img);
-
-			return message.reply({
-				embeds: [successEmbed]
-			});
 		};
 
-		return message.reply({
-			embeds: [errorEmbed]
+		// create an successEmbed
+		const embed = new Discord.MessageEmbed()
+			.setColor('RANDOM')
+			.setAuthor({
+				iconURL: user.displayAvatarURL(),
+				name: `@${user.username}`
+			})
+			.setDescription(`😡 **<@${message.author.id}> deu um tapa em <@${user.id}>**❗`)
+			.setTimestamp()
+			.setFooter({
+				text: 'Atualizado'
+			});
+
+		// check if user mentioned itself
+		if (message.author.id == user.id) {
+			embed.setDescription(`🤔 **<@${user.id}> quer tanto leva um tapa na raba**❓`);
+			endpoint = 'cringe';
+		};
+
+		// create an waiting embed
+		const waitingEmbed = new Discord.MessageEmbed()
+			.setColor('RANDOM')
+			.setDescription('⏳ Pensando...')
+			.setTimestamp()
+			.setFooter({
+				text: 'Atualizado'
+			});
+
+		// temporary message
+		const msg = await message.reply({
+			embeds: [waitingEmbed]
+		});
+
+		// get image
+		let img = await getImg(endpoint);
+
+		// final safety check (guarantee valid string)
+		if (!img || typeof img !== 'string') {
+			img = fallbackImage;
+		};
+
+		// set image
+		embed.setImage(img);
+
+		// edit immediately after ready
+		msg.edit({
+			embeds: [embed]
 		});
 	}
 };
