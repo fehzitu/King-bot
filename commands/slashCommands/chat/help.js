3293244c36507df1bf24b4182688406c27ce7ff7
fehzitu <fehzitujsdev@gmail.com>
2026-04-09 10:32:58 -0300

    minor changes on some embeds

--- commands/slashCommands/chat/help.js
+++ commands/slashCommands/chat/help.js
@@ -16,8 +16,8 @@ module.exports = {
             })
             .setImage('https://cdn.discordapp.com/attachments/1478819111906705430/1491724642799583322/images.jpeg?ex=69d8bc55&is=69d76ad5&hm=20d6a24780e97cda50b1b41b0721d16a856c588b144551bd7c3e26dfb7b3fb14&')
             .addFields([{
-                name: '🚩 Em **qualquer servidor que eu estiver** basta **você utilizar** meu comando base pra **abrir o menu e interagir comigo** no chat.',
-                value: '> 👇 __Utilize **k.menu** no chat!__'
+                name: '🥀 Em **qualquer servidor que eu estiver** basta **você utilizar** meu comando base pra **abrir o menu e interagir comigo** no chat.',
+                value: '> 👉 __Utilize **k.menu** no chat!__'
             }])
             .setTimestamp()
             .setFooter({
