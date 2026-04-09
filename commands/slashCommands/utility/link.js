fdc4a5f82deee37f5c9e173b4a09a6e9ee6f22b0
fehzitu <fehzitujsdev@gmail.com>
2026-04-09 10:19:29 -0300

    fix link slash command

--- commands/slashCommands/utility/link.js
+++ commands/slashCommands/utility/link.js
@@ -4,8 +4,8 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('info')
-        .setDescription('Mostra informações do bot!'),
+        .setName('link')
+        .setDescription('Link do nosso servidor de suporte!'),
     async execute(interaction) {
         // create an embed
         const embed = new Discord.MessageEmbed()
@@ -14,12 +14,11 @@ module.exports = {
                 iconURL: `${interaction.user.displayAvatarURL()}`,
                 name: `@${interaction.user.username}`
             })
-            .setTitle('Title')
-            .addFields([{
-                name: `Name`,
-                value: `Value`
-            }])
             .setImage('https://cdn.discordapp.com/attachments/1477290272638632068/1491764536322035802/yuliowo.gif?ex=69d8e17d&is=69d78ffd&hm=93ba3d94e4fa0d41eacb0ffc689604c26d884ee632a491e54c9c3e9b6cfd9e9f')
+            .addFields([{
+                name: `👑 **Drakay**, nosso servidor de suporte!`,
+                value: "🔗 **https://discord.gg/Wpgu4qXWUk**"
+            }])
             .setTimestamp()
             .setFooter({
                 text: 'Atualizado'
