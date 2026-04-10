46e5e72dfea268bf0d6ee5ce4a3386af4e7471be
fehzitu <fehzitujsdev@gmail.com>
2026-04-10 06:46:05 -0300

    fix

--- interaction/pages/menu/support.js
+++ interaction/pages/menu/support.js
@@ -20,7 +20,7 @@ module.exports = {
                 name: `@${user.username}`
             })
             .addFields([{
-                name: '🤖 **Ainda estou em desenvolvimento então pode ser que algo de errado ou inesperado aconteça. Se for o caso informe aos desenvolvedores bellzitu / dr3ssa**',
+                name: '🤖 **Ainda estou em desenvolvimento então pode ser que algo de errado ou inesperado aconteça. Se for o caso informe aos desenvolvedores bellzitu / karyarky**',
                 value: '☕ **Bora tomar um café?**'
             }])
             .setImage('https://cdn.discordapp.com/attachments/1477290272638632068/1491798480216264824/93f04b4b1103cc4e6410bb4f831acb6c.gif?ex=69d90119&is=69d7af99&hm=ca14d33797d593d0914af41ff129a00fc1f87c8ab1ce3a564058456fc749bb34&')
