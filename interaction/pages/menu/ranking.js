515ee929aab01c8f983d876b0c6deb1b155b0c62
fehzitu <fehzitujsdev@gmail.com>
2026-04-10 21:50:13 -0300

    ranking embed

--- interaction/pages/menu/ranking.js
+++ interaction/pages/menu/ranking.js
@@ -20,8 +20,8 @@ module.exports = {
                 name: `@${user.username}`
             })
             .addFields([{
-                name: '#1 bellzitu',
-                value: 'Lv10 | Xp2948 | R$100'
+                name: '🏆 **Ranking**',
+                value: '🥇 **#1 bellzitu**\n📈 **Lv10** | 💵 **R$100**'
             }])
             .setImage('https://cdn.discordapp.com/attachments/1477290272638632068/1492308028995928134/7C65091E-1577-4AE5-ABFB-31A55260A19D.gif?ex=69dadba7&is=69d98a27&hm=42efbc93ef98401702c281afd82e5ee7501499ee7ec6a435c7fc7428981a2ae0&')
             .setTimestamp()
