108a54c83032aa856713128e4973434ec74abc95
fehzitu <fehzitujsdev@gmail.com>
2026-04-12 19:50:02 -0300

    fix ranking embed

--- interaction/pages/menu/ranking.js
+++ interaction/pages/menu/ranking.js
@@ -35,8 +35,8 @@ module.exports = {
                 name: `@${user.username}`
             })
             .addFields([{
-                name: '🏆 **Ranking**',
-                value: '🥇 **#1 bellzitu**\n📈 **Lv10** | 💵 **R$100**'
+                name: '🏆: **Rankings**',
+                value: '**💰: Top dinheiro - ``Veja os 5 mais ricos``\n📈: Top nivel - ``Veja os 5 com mais níveis``\n💬: Top mensagens - ``Veja os 5 mais hypados``**'
             }])
             .setImage('https://cdn.discordapp.com/attachments/1477290272638632068/1492308028995928134/7C65091E-1577-4AE5-ABFB-31A55260A19D.gif?ex=69dadba7&is=69d98a27&hm=42efbc93ef98401702c281afd82e5ee7501499ee7ec6a435c7fc7428981a2ae0&')
             .setTimestamp()
@@ -49,6 +49,24 @@ module.exports = {
 
         // create some buttons inside a row
         row = new Discord.MessageActionRow().addComponents(
+            new Discord.MessageButton()
+                .setCustomId(`menu:page:money:${user.id}`)
+                .setLabel('💰')
+                .setStyle('PRIMARY')
+                .setDisabled(true),
+        
+            new Discord.MessageButton()
+                .setCustomId(`menu:page:level:${user.id}`)
+                .setLabel('📈')
+                .setStyle('PRIMARY')
+                .setDisabled(true),
+        
+            new Discord.MessageButton()
+                .setCustomId(`menu:page:messages:${user.id}`)
+                .setLabel('💬')
+                .setStyle('PRIMARY')
+                .setDisabled(true),
+            
             new Discord.MessageButton()
                 .setCustomId(`menu:page:home:${user.id}`)
                 .setLabel('🏠')
