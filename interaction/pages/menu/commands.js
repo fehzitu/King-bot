002e92b0a525553fb2e1e47c8625a152ee62b79b
fehzitu <fehzitujsdev@gmail.com>
2026-04-23 11:05:14 -0300

    fix embed font size

--- interaction/pages/menu/commands.js
+++ interaction/pages/menu/commands.js
@@ -1,13 +1,6 @@
 // discord implements
 const Discord = require('discord.js');
 
-// node file system
-const fs = require('fs');
-const path = require('path');
-
-// rules file
-const filePath = path.join(__dirname, 'complements/commands.json');
-
 module.exports = {
     name: 'commands',
     execute(ctx) {
@@ -20,10 +13,6 @@ module.exports = {
             return;
         };
 
-        // reading the file in real time
-        const rawData = fs.readFileSync(filePath, 'utf8');
-        const data = JSON.parse(rawData);
-
         // create an embed
         const embed = new Discord.MessageEmbed()
             .setColor('RANDOM')
@@ -31,7 +20,10 @@ module.exports = {
                 iconURL: user.displayAvatarURL(),
                 name: `@${user.username}`
             })
-            .addFields(data)
+            .addFields([{
+                name: "**📄 Lista completa dos comandos!**",
+                value: "**🇰.menu - ``Acessa o menu principal``**"
+            }])
             .setImage('https://cdn.discordapp.com/attachments/1477290272638632068/1496841750462074880/1_Awi84KRY_5cFk9P0cIhtrw.gif?ex=69eb5a03&is=69ea0883&hm=a9504132fccc4380b23edea6bda87c84e09445b4ae7da996ddbfb743c55b31f9')
             .setTimestamp()
             .setFooter({
