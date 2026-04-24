cf900bbc3027ee51851c78fd845c727fede1dcda
fehzitu <fehzitujsdev@gmail.com>
2026-04-24 01:32:33 -0300

    fix embed and money gain

--- interaction/pages/games/face.js
+++ interaction/pages/games/face.js
@@ -1,22 +1,10 @@
 // discord implements
 const Discord = require('discord.js');
 
-/*
-// node file system
-const path = require('path');
-
-// database json file
-const filePath = path.join(__dirname, '../../../users.json');
-
 // importing custom functions from other file
 const {
     defaultUser
 } = require(path.join(__dirname, '../../../functions/levelSystem.js'));
-const {
-    loadJson,
-    saveJson
-} = require(path.join(__dirname, '../../../functions/jsonHandler.js'));
-*/
 
 module.exports = {
     name: 'face',
@@ -34,7 +22,7 @@ module.exports = {
         const randomValue = Math.floor(Math.random() * 2);
 
         // embed itens
-        const titles = ['> 🟢 **Caiu __cara__ você ganhou R$100**', '> 🔴 **Caiu __coroa__ você perdeu R$100**'];
+        const titles = ['> 🟢 **Caiu __cara__ você ganhou R$100**', '> 🔴 **Caiu __coroa__ você perdeu R$50**'];
         const imgs = ['https://cdn.discordapp.com/attachments/1477290272638632068/1497067466361143326/2026-04-24-ganhou.gif?ex=69ec2c3a&is=69eadaba&hm=c5e0bcd210aaec5e246f9ee0ee0cad59a9841791ffab4d7f8054230a71219e8d&', 'https://cdn.discordapp.com/attachments/1477290272638632068/1497067466654879834/2026-04-24-perdeu.gif?ex=69ec2c3a&is=69eadaba&hm=078a35700ee10c5a714a8d58ab51991710cf3773de73bce50f9bac2b2c9dc305&'];
         const color = ['GREEN', 'RED'];
 
@@ -45,11 +33,17 @@ module.exports = {
         // load users database once
         const users = ctx.client.usersData;
 
+        // fix user
+        if (!users[userId]) {
+            // create new profile
+            users[userId] = defaultUser;
+        };
+
         // get user profile
         const profile = users[user.id];
 
         // create an embed
-        const embed = new Discord.MessageEmbed()
+        let embed = new Discord.MessageEmbed()
             .setColor(`${color[randomValue]}`)
             .setAuthor({
                 iconURL: user.displayAvatarURL(),
@@ -66,7 +60,7 @@ module.exports = {
             });
 
         // create some buttons inside a row
-        const row = new Discord.MessageActionRow().addComponents(
+        let row = new Discord.MessageActionRow().addComponents(
             new Discord.MessageButton()
                 .setCustomId(`${user.id}`)
                 .setLabel(`${btnSymbol[randomValue]}`)
@@ -78,6 +72,39 @@ module.exports = {
                 .setLabel('↩️')
                 .setStyle('PRIMARY')
         );
+        
+        // check user money
+        if (profile.rpg.money < 50) {
+            embed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setAuthor({
+                iconURL: user.displayAvatarURL(),
+                name: `@${user.username}`
+            })
+            .setDescription('❌ Saldo insuficiente!')
+            .setTimestamp()
+            .setFooter({
+                text: 'Atualizado'
+            });
+            
+            row = new Discord.MessageActionRow().addComponents(
+                new Discord.MessageButton()
+                    .setCustomId(`page:games:coinflip:${user.id}`)
+                    .setLabel('↩️')
+                    .setStyle('PRIMARY')
+            );
+
+            return {
+                embed,
+                components: [row]
+            };
+        };
+        
+        // user pay
+        if (profile.rpg.money >= 50) profile.rpg.money -= 50;
+        
+        // pay to user
+        if (randomValue == 0) profile.rpg.money += 100;
 
         return {
             embed,
