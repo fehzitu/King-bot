29741406c82122516cc30387875e7b05e7ce76cf
fehzitu <fehzitujsdev@gmail.com>
2026-04-24 00:34:42 -0300

    add color to buttons

--- interaction/pages/games/face.js
+++ interaction/pages/games/face.js
@@ -38,6 +38,10 @@ module.exports = {
         const imgs = ['https://cdn.discordapp.com/attachments/1477290272638632068/1497067466361143326/2026-04-24-ganhou.gif?ex=69ec2c3a&is=69eadaba&hm=c5e0bcd210aaec5e246f9ee0ee0cad59a9841791ffab4d7f8054230a71219e8d&', 'https://cdn.discordapp.com/attachments/1477290272638632068/1497067466654879834/2026-04-24-perdeu.gif?ex=69ec2c3a&is=69eadaba&hm=078a35700ee10c5a714a8d58ab51991710cf3773de73bce50f9bac2b2c9dc305&'];
         const color = ['GREEN', 'RED'];
 
+        // button itens
+        const btnColor = ['SUCCESS', 'DANGER'];
+        const btnSymbol = ['✔️', '❌'];
+
         // load users database once
         const users = ctx.client.usersData;
 
@@ -63,6 +67,12 @@ module.exports = {
 
         // create some buttons inside a row
         const row = new Discord.MessageActionRow().addComponents(
+            new Discord.MessageButton()
+                .setCustomId(`${user.id}`)
+                .setLabel(`${btnSymbol[randomValue]}`)
+                .setStyle(`${btnColor[randomValue]}`)
+                .setDisabled(true),
+
             new Discord.MessageButton()
                 .setCustomId(`page:games:coinflip:${user.id}`)
                 .setLabel('↩️')
