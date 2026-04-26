17b90228d78c7e237f04263ab482762c808f8e6b
fehzitu <fehzitujsdev@gmail.com>
2026-04-26 06:24:06 -0300

    fix details

--- interaction/pages/ranking/moneyRank.js
+++ interaction/pages/ranking/moneyRank.js
@@ -51,7 +51,7 @@ module.exports = {
             .setColor('RANDOM')
             .setAuthor({
                 iconURL: user.displayAvatarURL(),
-                name: `@${user.username}`
+                name: `@${user.username} Lv.${rpgUser.rpg.level} ${rpgUser.rpg.medals}`
             })
             .addFields([{
                 name: '**💰 Top dinheiro**',
