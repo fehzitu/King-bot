0578a56619137d64670676dd9992684e41ccc3c9
fehzitu <fehzitujsdev@gmail.com>
2026-04-29 22:15:44 -0300

    refactor

--- functions/levelSystem.js
+++ functions/levelSystem.js
@@ -1,24 +1,26 @@
-// Default profile user
-const defaultUser = {
-    profileCreatedAt: new Date().toISOString(),
-    rpg: {
-        level: 1,
-        xp: 0,
-        karma: '🌀 (Sem karma)',
-        money: 100,
-        medals: []
-    },
-    stats: {
-        messages: 0,
-        commands: 0
-    },
-    karma: {
-        positive: 0,
-        negative: 0
-    },
-    cooldowns: {
-        xp: 0
-    }
+// Default profile user (factory)
+function defaultUser() {
+    return {
+        profileCreatedAt: new Date().toISOString(),
+        rpg: {
+            level: 1,
+            xp: 0,
+            karma: '🌀 (Sem karma)',
+            money: 100,
+            medals: []
+        },
+        stats: {
+            messages: 0,
+            commands: 0
+        },
+        karma: {
+            positive: 0,
+            negative: 0
+        },
+        cooldowns: {
+            xp: 0
+        }
+    };
 };
 
 // calculate XP needed for next level
