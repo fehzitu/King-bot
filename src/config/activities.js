0a9d16560a1ebd69daad3fffeaef77310906a0e7
fehzitu <fehzitujsdev@gmail.com>
2026-04-30 20:15:31 -0300

    fix activities

---
+++ src/config/activities.js
@@ -0,0 +1,8 @@
+module.exports = [
+    'Olha pra tropa já joga a xereca',
+    'Se eu declarar minha grana toda eu mudo o PIB do país',
+    'Meus manos são da killa',
+    'Eu só falo da lobelia',
+    'Meu atirador é um black spy',
+    'Puta se eu puxo o gatilho só bye-bye'
+];
